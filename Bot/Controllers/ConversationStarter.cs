9c60cb97236ccfa2be0f38df59e930dda28d15b4
ekourtzanidis <s.kourtzanidis@outlook.com>
2017-05-24 15:22:03 +0300

    Utilise accessToken in GraphHelper and provide notification message

--- Bot/Controllers/ConversationStarter.cs
+++ Bot/Controllers/ConversationStarter.cs
@@ -1,13 +1,15 @@
 ﻿
 using Microsoft.Bot.Connector;
+using SampleAADV2Bot.Helpers;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SampleAADV2Bot.Controllers
 {
     public class ConversationStarter
     {
-        public static async Task Resume(string conversationId, string channelId, string toId, string toName, string fromId,string fromName, string serviceUrl)
+        public static async Task Resume(string conversationId, string channelId, string toId, string toName, string fromId,string fromName, string serviceUrl,string accessToken)
         {
             var userAccount = new ChannelAccount(toId, toName);
             var botAccount = new ChannelAccount(fromId, fromName);
@@ -26,7 +28,11 @@ namespace SampleAADV2Bot.Controllers
             message.From = botAccount;
             message.Recipient = userAccount;
             message.Conversation = new ConversationAccount(id: conversationId);
-            message.Text = "Hello, this is a notification";
+            var graphHelper = new GraphHelper(accessToken);
+            //graphHelper.Token = accessToken;
+            var userinfo = await graphHelper.GetUserInfo();
+            var meetingRoomsList = await graphHelper.GetMeetingRoomSuggestions();
+            message.Text = $"Hello {userinfo.DisplayName}. It seems you're making a lot of noise! {meetingRoomsList.First()} is available. You could continue there!";
             message.Locale = "en-Us";
             await connector.Conversations.SendToConversationAsync((Activity)message);
         }
